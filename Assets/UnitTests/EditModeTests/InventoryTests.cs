fb925b485be2f8a04ae903f19b1aff56ba9071af
dovjur <dovydas.jurgutis@ktu.edu>
2023-04-22 17:01:02 +0300

    Inventory Tests

---
+++ Assets/UnitTests/EditModeTests/InventoryTests.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class InventoryTests
+{
+    ItemObject itemObject = Resources.Load<ItemObject>("Items/0");
+    ItemObject itemObject1 = Resources.Load<ItemObject>("Items/1");
+    [Test]
+    public void AddItem_EmptyInventory_AddsItemToFirstSlot()
+    {
+        Inventory inventory = new Inventory();
+
+        inventory.AddItem(itemObject);
+
+        Assert.That(inventory.inventory[0].itemID == itemObject.ID);
+        Assert.That(inventory.inventory[0].stackSize == 1);
+    }
+
+    [Test]
+    public void AddItem_AlreadyHasItemInInventory_AddsToItemsStack()
+    {
+        Inventory inventory = new Inventory();
+        inventory.AddItem(itemObject);
+
+        inventory.AddItem(itemObject);
+
+        Assert.That(inventory.inventory[0].stackSize == 2);
+        Assert.That(inventory.inventory[0].itemID == itemObject.ID);
+    }
+
+    [Test]
+    public void AddItem_AlreadyHasItemAddNewItem_AddsItemToNextSlot()
+    {
+        Inventory inventory = new Inventory();
+        inventory.AddItem(itemObject);
+
+        inventory.AddItem(itemObject1);
+
+        Assert.That(inventory.inventory[0].itemID == itemObject.ID);
+        Assert.That(inventory.inventory[1].itemID == itemObject1.ID);
+    }
+
+    [Test]
+    public void RemoveItem_ItemsExists_RemovesItem()
+    {
+        Inventory inventory = new Inventory();
+        inventory.AddItem(itemObject);
+        inventory.AddItem(itemObject1);
+
+        inventory.RemoveItem(itemObject);
+
+        Assert.That(inventory.inventory[0].itemID != itemObject.ID);
+        Assert.That(inventory.inventory.Count == 1);
+    }
+
+    [Test]
+    public void RemoveItem_ItemDoesntExists_InventoryStaysTheSame()
+    {
+        Inventory inventory = new Inventory();
+        inventory.AddItem(itemObject);
+
+        inventory.RemoveItem(itemObject1);
+
+        Assert.That(inventory.inventory[0].itemID == itemObject.ID);
+    }
+}
